43a03efe192311cc7a0b57860a08c8f30dfba97d
caalix <mariocalix02@gmail.com>
2021-11-12 21:45:10 -0600

    Enviando avnace 2

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,19 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 
 import javax.swing.JOptionPane;
 
-
+/**
+ *
+ * @author mario
+ */
 public class Usuario {
     private String nombre, usuario, contrasena;
-    private int edad;
+    private int edad,num_cuenta;
+    private float saldo=4500;
 
     public Usuario() {
     }
 
-    public Usuario(String nombre, String usuario, String contrasena, int edad) {
+    public Usuario(String nombre, String usuario, String contrasena, int edad, int num_cuenta, float saldo) {
         this.nombre = nombre;
         this.usuario = usuario;
         this.contrasena = contrasena;
         this.edad = edad;
+        this.num_cuenta = num_cuenta;
+        this.saldo = saldo;
     }
 
     public String getNombre() {
@@ -45,15 +57,30 @@ public class Usuario {
     }
 
     public void setEdad(int edad) {
-        if(edad<18){
-            JOptionPane.showMessageDialog(null, "Debe ser mayor de 18 anios.");
-        }
         this.edad = edad;
+    }
+
+    public int getNum_cuenta() {
+        return num_cuenta;
+    }
+
+    public void setNum_cuenta(int num_cuenta) {
+        this.num_cuenta = num_cuenta;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
     }
 
     @Override
     public String toString() {
-        return "Usuario{" + "nombre=" + nombre + ", usuario=" + usuario + ", contrasena=" + contrasena + ", edad=" + edad + '}';
+        return "Usuario{" + "nombre=" + nombre + ", usuario=" + usuario + ", contrasena=" + contrasena + ", edad=" + edad + ", num_cuenta=" + num_cuenta + ", saldo=" + saldo + '}';
     }
+
+    
     
 }
--- src/Videojuego.java
+++ src/Videojuego.java
@@ -1,7 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 
 import java.util.Date;
 
-
+/**
+ *
+ * @author mario
+ */
 public class Videojuego {
     private String nombre;
     private Date anio;
